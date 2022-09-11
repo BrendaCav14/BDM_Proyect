7fbdcd51fb46aa346b33a1a6dd8acd83c62c4a56
BrendaCav14 <111449881+BrendaCav14@users.noreply.github.com>
2022-09-10 21:22:15 -0500

    Initial commit

---
+++ LandingPage.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styleindex.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+      <?php echo  "<title>SHOPPING</title>";?>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
+    </head>
+
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <a class="navbar-brand" style="color:white; font-family: Sofia, sans-serif; font-size:40px; margin-top:-15px" >Shopping ♥</a>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  
+ <li class="nav-item dropdown" style="font-size:15px;margin-left:60px">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        Categorias</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+
+  <li class="nav-item dropdown" style="font-size:15px;">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        Listas</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+<br>
+</br>
+<form class="form-inline" style="margin-left: 50px">
+    <input class="form-control mr-sm-2" type="search" placeholder="Buscador" aria-label="Search" style="width: 300px;font-size:15px;">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="color:#E7F3FF;font-size:15px;">Buscar</button>
+  </form>
+
+  </div>
+
+  <button type="button" class="btn btn-primary" style="font-size:15px;color:#7B1842;font-weight: bold;background-color:#F2933A;border-color:#F2933A;margin-right:10px">Registrate ya</button>
+  <button type="button" class="btn btn-link" style="text-decoration: underline;font-size:15px;">Iniciar Sesion</button>
+</nav>
+
+<div class="container" style="margin-top:10px;width:600px;height:300px">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" >
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+    <div class="item active" style="width:600px;height:300px">
+      <img src="https://img.blogs.es/anexom/wp-content/uploads/2018/11/romwe.jpg" alt="Imagen1">
+    </div>
+
+    <div class="item" style="width:600px;height:300px">
+      <img src="https://imagenes.elpais.com/resizer/mol1mG5sX4CnBrqyk1vrpJMdUr8=/1960x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/XKOZUW2JWH3CDT2TBXEPNHMWTQ.jpg" alt="Imagen2">
+    </div>
+
+    <div class="item" style="width:600px;height:300px">
+      <img src="https://img.global.news.samsung.com/mx/wp-content/uploads/2019/10/ODT-623_Samsung-Fest-Master-ONE-SAMSUNG_Baja6.jpg" alt="Imagen3">
+    </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<form class="frase" >
+<h1 style="margin-top:40px;margin-left:370px;color:#7B1842;font-family:'Acme', sans-serif;font-size:50px;border-color:black">
+¡ LO MAS VENDIDO ESTE 2022 !</h1>
+</form>
+<br></br>
+
+
+<div class="card" style="width: 18rem; margin-left:300px">
+  <img src="https://revistabyte.es/wp-content/uploads/2020/05/1-323x360.jpg.webp" class="card-img-top" alt="Imagen1">
+  <div class="card-body">
+    <h5 class="card-title" style="font-size:20px">Control Remoto</h5>
+    <p class="card-text">Escelente calidad de control remoto para realizar streams de la marca PANASONIC</p>
+    <p style="font-size:15px">Precio: $200.00</p>
+    <a href="#" class="btn btn-primary">Ver producto</a>
+  </div>
+</div>
+
+
+<div class="row">
+  <div class="col-lg-6  col-md-18 md-6">
+    <div class="card" style="width: 18rem; margin-left:300px">
+    <img src="https://revistabyte.es/wp-content/uploads/2020/05/1-323x360.jpg.webp" class="card-img-top" alt="Imagen1">
+      <div class="card-body">
+      <h5 class="card-title" style="font-size:20px">Control Remoto</h5>
+    <p class="card-text">Escelente calidad de control remoto para realizar streams de la marca PANASONIC</p>
+    <p style="font-size:15px">Precio: $200.00</p>
+    <a href="#" class="btn btn-primary">Ver producto</a>
+      </div>
+    </div>
+  </div>
+  <div>
+ 
+</body>
+</html>
